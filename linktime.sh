376b658e8727ec483af061b1ce31928fb083d8d6
Rudolfs Tilgass <rudzha@gmail.com>
2015-08-24 23:55:12 +0300

    Symlink fix, some color, bash history, feh, ckb, set dpi, set secondary display only if it's connected, etc

--- linktime.sh
+++ linktime.sh
@@ -1,4 +1,4 @@
-# /bin/sh
+#!/bin/sh
 
 WDIR=$(pwd)
 
@@ -7,11 +7,16 @@ mkdir -p $HOME/.i3
 ln -sf $WDIR/i3/i3.conf $HOME/.i3/config
 
 mkdir -p $HOME/.config/i3status
-ln -sf $WDIR/i3/i3status.conf $HOME/.config/i3status
+ln -sf $WDIR/i3/i3status.conf $HOME/.config/i3status/config
 
 sudo ln -sf $WDIR/i3/lock.sh /usr/bin/lock
 
 ### X ###
 ln -sf $WDIR/X/xinitrc $HOME/.xinitrc
 ln -sf $WDIR/X/Xresources $HOME/.Xresources
+
 xrdb $HOME/.Xresources
+
+### Bash ye
+ln -sf $WDIR/bash/bashrc $HOME/.bashrc
+ln -sf $WDIR/bash/inputrc $HOME/.inputrc
